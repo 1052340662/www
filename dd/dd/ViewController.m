ff536171e208c0a1859a569c95718af3b6bbe420
wuyanlei <1052340662@qq.com>
2016-10-28 13:34:10 +0800

    ff

--- dd/dd/ViewController.m
+++ dd/dd/ViewController.m
@@ -19,6 +19,8 @@
     NSLog(@"dfhdf");
     // Do any additional setup after loading the view, typically from a nib.
     self.view.backgroundColor = [UIColor redColor];
+    int a = 0;
+
 }
 
 - (void)didReceiveMemoryWarning {
